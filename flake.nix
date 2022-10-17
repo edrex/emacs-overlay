fdf641f77e3e28ecc64e1f5826c74705b788e86b
Eric Drechsel <ericdrex@gmail.com>
2022-10-17 11:15:57 -0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Bleeding edge Emacs overlay";
 
   nixConfig = {
-    extra-substituters = "nix-community.cachix.org";
+    extra-substituters = "https://nix-community.cachix.org";
     extra-trusted-public-keys = "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
   };
   
